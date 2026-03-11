52d17b2239148a61d1e697a6132eaba005f2cf6d
piyushbhardawaj1324 <piyushbhardawaj1@gmail.com>
2026-03-11 18:36:33 +0530

    Update __main__.py

--- AsuXMusic/__main__.py
+++ AsuXMusic/__main__.py
@@ -1,26 +1,22 @@
 import asyncio
 import importlib
-import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 from pytgcalls import idle
-
 from AsuXMusic import BOT_USERNAME, bot, call_py
 from AsuXMusic.Modules import ALL_MODULES
 
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 loop = asyncio.get_event_loop()
-
 
 async def AsuX_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("AsuXMusic.Modules." + all_module)
-    print(f"@{BOT_USERNAME} is ready to run ")
+    print(f"@{BOT_USERNAME} is ready to run")
     await bot.start()
-    print("assistant started ")
+    print("assistant started")
     await call_py.start()
     await idle()
-    print(f"ɢᴏᴏᴅʙʏᴇ!\nStopping @{BOT_USERNAME}")
+    print(f"GOODBYE!\nStopping @{BOT_USERNAME}")
     await bot.stop()
-
 
 if __name__ == "__main__":
     loop.run_until_complete(AsuX_boot())
